7871ef9c88dae00bbdb3a60d5d2935c1dbf84839
Fiza <131092372+Fizahere@users.noreply.github.com>
2024-10-21 15:26:25 +0500

    [login user api]

--- middlewares/authMiddleWare.js
+++ middlewares/authMiddleWare.js
@@ -1,15 +1,22 @@
 import jwt from 'jsonwebtoken';
 
 export const authenticateToken = (req, res, next) => {
-    const token = req.headers.authorization;
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Access denied, token missing!' });
+    }
+
+    const token = authHeader.split(' ')[1]; 
+
     if (!token) {
         return res.status(401).json({ message: 'Access denied, token missing!' });
     }
+
     try {
-        const decoded = jwt.verify(token, "aurora");
+        const decoded = jwt.verify(token, "aurora"); 
         req.user = decoded.user;
         next();
     } catch (error) {
-        res.status(403).json({ message: 'Invalid token' });
+        res.status(403).json({ message: 'Invalid token', error: error.message });
     }
 };
